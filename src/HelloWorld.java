6301b1933eae07ab673244f4216b1e849f9ebb3e
AlexRios <riosalexandra91@gmail.com>
2022-06-03 10:53:01 -0500

    finished HelloWorld exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+//public means the class- we are defining our class
+//static is a keyword we'll learn more about later
+//void means there is no return type and that it is just going to preform a task
+//shortcut for "public static main void" is "psvm"
+//system.out line means this is essentially our console.log- it prints out what we see in the console (shortcut for this is 'SOUT')
+
+
